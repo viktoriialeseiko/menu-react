1a597e44ffea2f2aac8da833da0f378579291517
viktoriialeseiko <vikyshaaa1@gmail.com>
2022-02-17 14:42:58 -0600

    categories are separated in a menu

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
     const [menuItems, setMenuItems] = useState(items);
-    const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState(allCategories);
 
     const filterItems = (category) => {
         if (category === 'all') {
